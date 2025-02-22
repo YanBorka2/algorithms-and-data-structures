00651b5c47ce9590d1e4b6243ccf8bbc717f5dfd
BorkaYan <borka.yan@student.uzhnu.edu.ua>
2025-02-22 16:51:45 +0300

    first commit

---
+++ LabJob1/Stack.js
@@ -0,0 +1,65 @@
+class Stack {
+    constructor(){
+        this.items = [];
+    }
+    
+    push(element) {
+        this.items.push(element);
+        console.log(`Елемент ${element} в стеку`);
+    }
+
+
+    pop() {
+        if (this.Empty()) {
+          console.log("Стек порожній");
+          return;
+        }
+        const deletElement = this.items.pop();
+        console.log(`Елемент ${deletElement} видалено з стеку.`);
+    }
+
+    Empty(){
+       return(this.items.length === 0)
+    }
+
+    Clear(){
+        this.items = [];
+        console.log("Стек очищено");
+    }
+
+    peek() {
+        if (this.Empty()) {
+          console.log("Стек порожній.");
+          return;
+        }
+        console.log(`Верхній елемент: ${this.items[this.items.length - 1]}`);
+    }
+
+    viewAllElement() {
+        if (this.Empty()) {
+          console.log("Стек порожній.");
+          return;
+        }
+        console.log("Елементи стеку:", this.items.join(", "));
+    }
+}
+
+
+const stack = new Stack();
+
+
+stack.push(3);
+stack.push(42);
+stack.push(70);
+
+stack.viewAllElement(); // Перегляд елементів
+
+stack.peek(); // Перегляд верхнього елемента
+
+stack.pop();  // Видалення елемента
+
+stack.viewAllElement(); // Перегляд елементів після видалення
+
+stack.Clear(); // Очистка стеку
+
+stack.viewAllElement(); // Перегляд елементів після очищення
